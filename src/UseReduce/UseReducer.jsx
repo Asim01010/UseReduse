ef03de6e3aa0ab8a28b74a6122822df22b6055b8
unknown <masimqw4hd@gmail.com>
2025-03-20 23:57:40 +0500

    bring me to

--- src/UseReduce/UseReducer.jsx
+++ src/UseReduce/UseReducer.jsx
@@ -18,6 +18,7 @@ const UseReducer = () => {
         error: false,
         success: true,
         message: "Your Command Done",
+        data: [...state.data, action.payload],
       };
     }
 
@@ -44,15 +45,14 @@ const UseReducer = () => {
     e.preventDefault();
     if (!name) {
       dispatch({ type: "INVALID_STATE" });
-    }
-    if (name) {
-      dispatch({ type: "ACCEPTED" });
+    } else {
+      dispatch({ type: "ACCEPTED", payload: { name, id: Date.now() } });
     }
   };
 
   setInterval(() => {
     dispatch({ type: "RESET" });
-  }, 3000);
+  }, 9000);
 
   return (
     <>
@@ -66,7 +66,7 @@ const UseReducer = () => {
             className="w-full border-4 outline-0 border-b-gray-400 border-t-0 border-l-0 border-r-0  my-3 "
           />
           {state.error && <p className="text-red-500">access denied</p>}
-          {/* {state.error || <p className="text-green-500">succefully add</p>} */}
+          {state.success && <p className="text-green-500">succefully add</p>}
 
           <button
             onClick={btnHandle}
@@ -75,6 +75,16 @@ const UseReducer = () => {
             Submit
           </button>
         </form>
+      </div>
+
+      <div className="container mx-auto grid grid-cols-3">
+        {state?.data?.map((item, index) => {
+          return (
+            <div className="container mx-auto flex justify-between items-center">
+              <div className="p-5 shadow-xl">{item.name}</div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
